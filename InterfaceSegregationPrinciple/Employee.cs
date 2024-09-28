1e8762b49d2f4a06f5d18a3bad5152d985559d6e
ganeshpaiya <ganesanhere4u@gmail.com>
2024-09-28 15:22:19 +0530

    Add project files.

---
+++ InterfaceSegregationPrinciple/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+    interface Isalary
+    {
+        public Double basesalary();
+        public Double hrbasesalary();
+       
+    }
+
+    interface Ivarablepay
+    {
+        public Double variavblesalary();  // Variable pay need only design class so i createsd on new interface and inhert permentemp class
+    }
+
+    public class permentEmployee : Isalary, Ivarablepay  //here achieved  SOLID Design pattan
+    {
+        public double basesalary()
+        {
+            return 12300.233;
+        }
+
+        public double hrbasesalary()
+        {
+            return 14300.233;
+        }
+
+        public double variavblesalary()
+        {
+            return 10300.233;
+        }
+    }
+
+    public class ContractEmployee : Isalary
+    {
+        public double basesalary()
+        {
+            return 10000.233;
+        }
+
+        public double hrbasesalary()
+        {
+            return 10200.233;
+        }
+
+        //public double variavblesalary()  
+        //{
+        //    return 40200.233;
+        //}
+    }
+
+}
---
+++ InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InterfaceSegregationPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            permentEmployee _pemp = new permentEmployee();
+            Console.WriteLine("Salary Of Permand emp:" + (_pemp.basesalary() + _pemp.hrbasesalary() + _pemp.variavblesalary()));
+            ContractEmployee _cemp = new ContractEmployee();
+            Console.WriteLine("Salary Of Contract emp:" + (_pemp.basesalary() + _pemp.hrbasesalary()));//Here i dnt want Contract employee variable pay 
+        }
+    }
+}
